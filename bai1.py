62b91ed675e71c82a572e9a0effd469693dceb17
vuxuanbach46-cpu <vuxuanbach46@gmail.com>
2026-05-20 12:51:58 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -6,7 +6,7 @@ if heart_rate > 120:
   print("Priority: RED - Critical condition! Immediate action required.")
 elif heart_rate > 100:
   print("Priority: YELLOW - Abnormal. Monitor closely.")
-elif heart_rate > 60:
+elif heart_rate < 60:
   print("Priority: BLUE - Bradycardia. Require ultrasound.")
 else:
   print("Priority: GREEN - Stable. Please wait in the lobby.")
